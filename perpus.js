be2a74f32834dfe5efe487293dc42f1745146b67
qibal <iqbalherlambang.39@gmail.com>
2023-11-09 23:06:06 +0700

    upload 1

---
+++ perpus.js
@@ -0,0 +1,7 @@
+        function lainnya() {
+        document.getElementById("lihatsemua").style.display = "block";
+      }
+
+      function info() {
+        alert("halaman sedang di di kembangkan");
+      }
